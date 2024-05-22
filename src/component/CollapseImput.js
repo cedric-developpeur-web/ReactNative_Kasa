2e3532c07c68341286c95923d92e57a6ef3993e3
cocq cedric <cedric-developpeur-web@laposte.net>
2024-05-22 15:56:56 +0200

    création du menu de la page about + style

--- src/component/CollapseImput.js
+++ src/component/CollapseImput.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
-const Collapse = () => {
-  return <div id="collapse_imput"></div>;
+const Collapse = ({ title, description }) => {
+  return (
+    <section id="menu">
+      <div className="collapse_imput">
+        <h4>{title}</h4>
+      </div>
+      <p>{description}</p>
+    </section>
+  );
 };
 
 export default Collapse;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,5 @@
-import React from 'react';
+// import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../component/Header';
 import Footer from '../component/Footer';
 import Banner from '../component/Banner';
@@ -7,6 +8,23 @@ import CollapseImput from '../component/CollapseImput';
 const About = () => {
   const srcPictureMontain = './asset/picture/picture_montain.png';
   const altPictureMontain = 'plage montagne';
+
+  const [collapses, setCollapses] = useState([]);
+  useEffect(() => {
+    // vient recuperer les données du fichier json
+    const fetchData = async () => {
+      const reponse = await fetch('/fichier_json/collapse.json');
+      if (!reponse.ok) {
+        console.error('error');
+        return;
+      }
+      // vient conserver les données recuperer du fichier json
+      const data = await reponse.json();
+      // met a jour l'etat (annonces) avec les donnée du fichier json lors du permier rendu du component
+      setCollapses(data);
+    };
+    fetchData();
+  }, []);
   return (
     <div>
       <Header />
@@ -16,10 +34,14 @@ const About = () => {
           alt={altPictureMontain}
           removeTagP={false}
         />
-        <CollapseImput />
-        <CollapseImput />
-        <CollapseImput />
-        <CollapseImput />
+        {/* je vient crée 4 collapse en leur indiquant de recupérer les données du tableau qui est stocke dans const collapses */}
+        {collapses.map((collapse, index) => (
+          <CollapseImput
+            key={index}
+            title={collapse.title}
+            description={collapse.description}
+          />
+        ))}
       </main>
       <Footer />
     </div>
