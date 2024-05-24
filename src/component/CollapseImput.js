32567b0f5664b36b8c2b56986ccb4e00e2f2a302
cocq cedric <cedric-developpeur-web@laposte.net>
2024-05-24 09:53:12 +0200

    ajoute de la font icone pour le chevron up  (reste animation)

--- src/component/CollapseImput.js
+++ src/component/CollapseImput.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
 
 const Collapse = ({ title, description }) => {
   return (
     <section id="menu">
       <div className="collapse_imput">
+        <FontAwesomeIcon className="cheveron_up" icon={faChevronUp} />
         <h4>{title}</h4>
       </div>
       <p>{description}</p>
