c1ba3e4426208e269d9cecda1af5ffe88555bb95
cocq cedric <cedric-developpeur-web@laposte.net>
2024-06-12 15:37:47 +0200

    modification des icones chevron + etoile en svg

--- src/component/Collapses.js
+++ src/component/Collapses.js
@@ -12,14 +12,25 @@ const Collapse = ({ title, description }) => {
       <div className="collapses">
         <h4>{title}</h4>
         {/* si je clic sur l'icone du chevron je lui applique une rotation de -180deg avec une transition de 0.5 */}
-        <i
-          className={`fa-solid fa-chevron-up`}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className=" fa-chevron-up"
           style={{
             transform: opening ? 'rotate(-180deg)' : 'none',
             transition: 'transform 0.5s ease',
           }}
           onClick={toggleDescription}
-        ></i>
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m4.5 15.75 7.5-7.5 7.5 7.5"
+          />
+        </svg>
       </div>
       <div>
         {/* quand je clique sur le chevron si opening et false la classe close s'applique mais si opening et true la classe open s'applique */}
